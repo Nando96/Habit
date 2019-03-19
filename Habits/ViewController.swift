c27b1562be87084455b941594f64ba68ce8df36a
fernando <nandomendo96@gmail.com>
2019-03-18 23:08:00 -0700

    slight update

--- Habits/ViewController.swift
+++ Habits/ViewController.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController.swift
+//  Habits
+//
+//  Created by Fernie on 3/13/19.
+//  Copyright © 2019 zxzx. All rights reserved.
+//
+
 import UIKit
 import EventKit
 
--- Habits/ViewController2.swift
+++ Habits/ViewController2.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController2.swift
+//  Habits
+//
+//  Created by Fernie on 3/13/19.
+//  Copyright © 2019 zxzx. All rights reserved.
+//
+
 import UIKit
 import EventKit
 var list :[String] = []
